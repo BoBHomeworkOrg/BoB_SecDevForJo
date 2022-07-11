53fe1388c1dcf268ca27e211fa8b60410e573a81
arad4228 <arad4228@gmail.com>
2022-07-11 20:21:12 +0900

    Add internal outer study

--- 7.11/main.py
+++ 7.11/main.py
@@ -1,10 +1,16 @@
-def add(v1,v2):
+v0 = 1000
+
+
+def add(v1, v2):
+    v0 = 90
+    print(v0)
     return v1+v2
 
 
 a = int(input("V1:"))
 b = int(input("V2:"))
 
+print(v0)
 print("V1 + V2 =" + str(add(a,b)))
 
 
